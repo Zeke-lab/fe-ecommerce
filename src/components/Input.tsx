b992ee4c792ff7c31a067ff4ed228b9538af00f8
AungKyawPhyo1142 <aungkyawphyo142.edu@gmail.com>
2025-12-24 22:04:49 +0700

    feat: initial project setup

---
+++ src/components/Input.tsx
@@ -0,0 +1,89 @@
+import clsx from "clsx";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    className?: string;
+    error?: string;
+    hint?: string;
+}
+
+type InputState = {
+    inputType: React.HTMLInputTypeAttribute;
+    showPassword: boolean;
+}
+
+/*
+    <input type="text" name="email" value="" placeholder />
+*/
+
+const Input: React.FC<InputProps> = (props) => {
+    const {
+        className,
+        error,
+        hint,
+        type,
+        name,
+        value,
+        disabled,
+        ...rest
+    } = props
+
+    const [inputState, setInputState] = useState<InputState>({
+        inputType: type || 'text',
+        showPassword: false
+    })
+
+    const onEyeIconClick = () => {
+        setInputState((prevState) => ({
+            inputType: prevState.inputType === 'password' ? 'text' : 'password',
+            showPassword: !prevState.showPassword
+        }))
+    }
+
+
+    const handlePasswordDisplayIcon = () => {
+        return inputState.showPassword ? (
+            <Eye
+                onClick={onEyeIconClick}
+                className="absolute right-4 cursor-pointer"
+                size={20}
+                strokeWidth={2}
+                color={'black'}
+            />) : (
+            <EyeOff
+                onClick={onEyeIconClick}
+                className="absolute right-4 cursor-pointer"
+                size={20}
+                strokeWidth={2}
+                color={'black'}
+            />
+        )
+
+    }
+
+    return (
+        <div className="w-full">
+            <div className="relative flex items-center">
+                <input
+                    {...rest}
+                    name={name}
+                    value={value}
+                    onChange={props.onChange}
+                    type={inputState.inputType}
+                    disabled={disabled}
+                    className={clsx(
+                        "min-h-12.5 w-full rounded-md border-2 px-3 py-2 text-base transition-all duration-300 ring-0 placeholder:text-zinc-400 outline-none focus:border-black",
+                        error ? 'border-red-500' : 'border-gray-500',
+                        disabled && 'opacity-50 cursor-not-allowed bg-gray-300',
+                        type === 'password' && 'pr-10',
+                        className
+                    )}
+                />
+                {type === 'password' && handlePasswordDisplayIcon()}
+            </div>
+        </div>
+    )
+}
+
+export default Input;
---
+++ src/routes/path.ts
@@ -0,0 +1,18 @@
+export const AppConstantRoutes = {
+    path: {
+        get auth() {
+            return {
+                get default() {
+                    return '/auth'
+                },
+                get login() {
+                    return `${this.default}/login`
+                },
+                get register() {
+                    return `${this.default}/register`
+                }
+            }
+        }
+    }
+}
+
